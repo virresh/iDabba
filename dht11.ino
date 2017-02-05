08f7e932082536298d1f5b7e8b3e3f9b8da6fa56
mittalshravika <shravika16093@iiitd.ac.in>
2017-02-05 19:22:55 +0530

    Add files via upload

---
+++ dht11.ino
@@ -0,0 +1,17 @@
+#include <dht.h>
+dht DHT;
+#define DHT11_PIN 7
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(1000);
+
+}
