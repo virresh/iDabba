233965ba50bb27f6653ac6d0c4a20a8fc1518459
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-03-11 22:56:59 +0530

    Basic Backend Set-up of flask. This is just to demonstrate how can we use REST api for our purposes. Login Procedures have been excluded.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('home.html')
+
+#example of using rest api for use with our website
+@app.route("/api/iname")
+def iname():
+    #idealy this will run some other scripts and return the name of item kept in iDabba
+    names= ['banana','orange','garlic','apple','potato']
+    x = names[random.randint(0,len(names)-1)]
+    return x
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+
