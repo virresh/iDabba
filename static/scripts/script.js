94090460c0227c8b0f8efe5c09e47ca5d875c703
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-03-11 22:56:59 +0530

    Basic Backend Set-up of flask. This is just to demonstrate how can we use REST api for our purposes. Login Procedures have been excluded.

---
+++ static/scripts/script.js
@@ -0,0 +1,29 @@
+new Vue({
+	delimiters: ['${', '}'] ,
+	el: '#stats',
+	data: {
+		iname:''
+	},	
+
+	http: {
+			emulateJSON: true,
+			emulateHTTP: true
+	},
+
+	mounted: function() {
+		this.iname = "Fetch Data. It will appear here.";
+	},
+
+	methods: {														
+		itemname: function() {
+			this.$http.get('/api/iname')
+			.then(function (x) {
+				this.iname = x.body;
+				//console.log(x);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+		}
+	}
+});
