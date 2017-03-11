3e0b85196642086bbcf5590b32767ca8a17c411e
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-03-11 23:17:39 +0530

    Added JSON return type from server.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 import random
 app = Flask(__name__)
 
@@ -10,9 +10,10 @@ def main():
 @app.route("/api/iname")
 def iname():
     #idealy this will run some other scripts and return the name of item kept in iDabba
+    #returns json data
     names= ['banana','orange','garlic','apple','potato']
     x = names[random.randint(0,len(names)-1)]
-    return x
+    return jsonify({'objectName':x})
 
 if __name__ == "__main__":
 	app.run(debug=True)
