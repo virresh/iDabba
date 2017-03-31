049377cd1e8c44bedb3d998d31bdd0e38202f73d
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-03-31 15:20:19 +0530

    Stable Status

--- app.py
+++ app.py
@@ -15,7 +15,40 @@ def iname():
     x = names[random.randint(0,len(names)-1)]
     return jsonify({'objectName':x})
 
+@app.route("/api/temp/<string:data>")
+def tempDataLoad(data):
+	print data
+	fo = open("dataLog.txt","a+")
+	fo.write(data)
+	fo.write('\n')
+	fo.close()
+	return "recieved"
+
+@app.route("/api/getrd")
+def returnData():
+	fo = open("dataLog.txt")
+	y = ""
+	for x in fo:
+		y = x
+	return y
+# @app.route("/api/humidity/<string:data>") 
+# def humidityDataLoad(data):
+# 	print data
+# 	return "recieved"
+
+# @app.route("/api/rfid/<string:data>")
+# def rfidDataLoad(data):
+# 	print data
+# 	return "recieved"
+
+# @app.route("/api/weight/<string:data>")
+# def weightDataLoad(data):
+# 	print data
+# 	return "recieved"
+
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host='0.0.0.0',port=8080) 
+	# ,debug=True
 
 
